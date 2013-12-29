24c841445c8aefb9dca52788d506a75ca3e4bd62
Welles Robinson <wir963@gmail.com>
2013-12-28 23:08:56 -0600

    Initial Commit

---
+++ PhilanthroFeed/PFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PFAppDelegate.h
+//  PhilanthroFeed
+//
+//  Created by Welles Robinson on 12/28/13.
+//  Copyright (c) 2013 Welles Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhilanthroFeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhilanthroFeed
+//
+//  Created by Welles Robinson on 12/28/13.
+//  Copyright (c) 2013 Welles Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PFAppDelegate class]));
+    }
+}
